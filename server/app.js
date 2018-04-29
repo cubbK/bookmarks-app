b31b1d70f17bc66de73c76c3c06ad15c193fda8a
Dan <danpopa1998@gmail.com>
2018-04-29 22:55:27 +0300

    add mongoose

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://' + process.env.MONGO_DB);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
