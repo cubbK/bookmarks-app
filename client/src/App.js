e7566a50a32dd2c7284b2615be388a6cb29e40b5
Dan <danpopa1998@gmail.com>
2018-06-22 00:45:44 +0200

    add rebass and add first component to storybook

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// @flow
+import React, { Component } from 'react'
+import { Provider, Heading, Button } from 'rebass'
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  * { box-sizing: border-box; }
+  body { margin: 0; }
+`
 
 class App extends Component {
   render() {
     return (
-      <div>yo</div>
-    );
+      <Provider>
+        <div>yo</div>
+        <Heading>Hello</Heading>
+        <Button>Rebass</Button>
+      </Provider>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ client/src/components/Introduction.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Heading } from 'rebass'
+
+const Introduction = () => 
+  <header>
+    <Heading>Hello</Heading>
+  </header>
+
+  export default Introduction
