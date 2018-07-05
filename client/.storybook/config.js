65c86a6a337d9f18b265f7eb059ffb3db2131c64
ospite@nettunoroma <ospite@nettunoroma>
2018-07-05 18:36:45 +0200

    add theme

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,7 +1,14 @@
-import { configure } from '@storybook/react';
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react';
+import theme from '../src/theme/theme'
+import { MuiThemeProvider } from '@material-ui/core/styles';
+
+const addTheme = storyFn => <MuiThemeProvider theme={theme}>{storyFn()}</MuiThemeProvider>
+addDecorator(addTheme)
 
 function loadStories() {
   require('../src/stories');
 }
+
 
 configure(loadStories, module);
---
+++ client/src/theme/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#121316',
+        },
+        secondary: {
+            main: "#f22b29"
+        },
+        error: {
+            main: "#545454"
+        }
+    },
+});
+
+export default theme
