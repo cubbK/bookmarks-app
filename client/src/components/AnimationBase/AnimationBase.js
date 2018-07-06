45b4ed8943fe5308f40164af2c0e5147b6cf2882
Dan <danpopa1998@gmail.com>
2018-07-06 18:18:13 +0200

    start writing main animation comp with react spring

--- client/src/components/AnimationBase/AnimationBase.js
+++ client/src/components/AnimationBase/AnimationBase.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import { Spring } from 'react-spring'
+import styled from 'styled-components'
+import { secondaryColor } from 'theme/theme'
+
+const Circle = styled.div`
+  border-radius: 50%;
+  background: ${props => props.color ? props.color : 'black'}
+  width: ${props => props.width ? props.width : '25vw'}
+  height: ${props => props.height ? props.height : '25vw'} 
+`
 
 const AnimationBase = props =>
-  <div>123</div>
+  <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+    {styles =>
+      <div style={styles}>
+        <Circle color={secondaryColor} />
+      </div>}
+  </Spring>
 
 export default AnimationBase
--- client/src/theme/theme.js
+++ client/src/theme/theme.js
@@ -1,17 +1,21 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
+export const mainColor = '#121316'
+export const secondaryColor = '#f22b29'
+export const errorColor = '#545454'
+
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#121316',
-        },
-        secondary: {
-            main: "#f22b29"
-        },
-        error: {
-            main: "#545454"
-        }
+  palette: {
+    primary: {
+      main: mainColor,
     },
+    secondary: {
+      main: secondaryColor
+    },
+    error: {
+      main: errorColor
+    }
+  },
 });
 
 export default theme
