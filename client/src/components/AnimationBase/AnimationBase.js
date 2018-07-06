9dee30cdf726c87484d83e9f2b1b2bfb32d2a704
Dan <danpopa1998@gmail.com>
2018-07-06 20:03:11 +0200

    add notebook svg as a react component

--- client/src/components/AnimationBase/AnimationBase.js
+++ client/src/components/AnimationBase/AnimationBase.js
@@ -2,19 +2,13 @@ import React from 'react'
 import { Spring } from 'react-spring'
 import styled from 'styled-components'
 import { secondaryColor } from 'theme/theme'
-
-const Circle = styled.div`
-  border-radius: 50%;
-  background: ${props => props.color ? props.color : 'black'}
-  width: ${props => props.width ? props.width : '25vw'}
-  height: ${props => props.height ? props.height : '25vw'} 
-`
+import Notebook from './Notebook'
 
 const AnimationBase = props =>
   <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
     {styles =>
       <div style={styles}>
-        <Circle color={secondaryColor} />
+        <Notebook />
       </div>}
   </Spring>
 
