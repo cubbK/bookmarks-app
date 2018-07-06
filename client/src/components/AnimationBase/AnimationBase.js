1f77387b91f91ac777930f1674eba8453b873a2e
Dan <danpopa1998@gmail.com>
2018-07-06 17:12:55 +0200

    add animation base component

---
+++ client/src/components/AnimationBase/AnimationBase.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const AnimationBase = props =>
+  <div>123</div>
+
+export default AnimationBase
---
+++ client/src/components/AnimationBase/AnimationBase.stories.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { storiesOf} from '@storybook/react'
+import AnimationBase from './AnimationBase'
+
+export default storiesOf(`AnimationBase`, module)
+  .add(`stateless`, () => <AnimationBase />)
