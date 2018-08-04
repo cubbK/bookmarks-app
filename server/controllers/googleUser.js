7ac4229f8240d06b6e0c5c1e488068eb08cdfe0b
Dan <danpopa1998@gmail.com>
2018-08-04 19:29:19 +0300

    create user in db and return the response object

--- server/controllers/googleUser.js
+++ server/controllers/googleUser.js
@@ -1,29 +1,40 @@
-const Router = require('koa-router')
-const User = require('../models/user')
-const jwtDecode = require('jwt-decode')
+const Router = require("koa-router");
+const User = require("../models/user");
+const jwtDecode = require("jwt-decode");
 
 const router = new Router({
-  prefix: '/googleUser'
-})
+  prefix: "/googleUser"
+});
 
-const getGoogleDataFromAuthorizationCode = require('../helpers/getGoogleDataFromAuthorizationCode.js')
-router.use(getGoogleDataFromAuthorizationCode)
+const getGoogleDataFromAuthorizationCode = require("../helpers/getGoogleDataFromAuthorizationCode.js");
+router.use(getGoogleDataFromAuthorizationCode);
 
-router.get('/', async (ctx, next) => {
+router.get("/", async (ctx, next) => {
   // const user = await User.findOrCreate(ctx.googleTokenData)
-  console.log(ctx.googleTokens)
-  
-  const refreshToken = ctx.googleTokens.refresh_token
-  const accessToken = ctx.googleTokens.access_token
+  console.log(ctx.googleTokens);
+  try {
+    const refreshToken = ctx.googleTokens.refresh_token;
+    const accessToken = ctx.googleTokens.access_token;
 
-  const userInfo = jwtDecode(ctx.googleTokens.id_token)
-  const userId = userInfo.sub
-  const userName = userInfo.given_name
+    const userInfo = jwtDecode(ctx.googleTokens.id_token);
+    const userId = userInfo.sub;
+    const userName = userInfo.given_name;
 
-  console.log('decoded token: ', decodedToken)
-  ctx.response.body = '123'
-  // ctx.response.body = JSON.stringify(user)
-  return next()
-})
+    const retrievedUser = await User.findOrCreateByGoogleId(userId);
+    User.setUserField(userId, "googleAccessToken", accessToken);
 
-module.exports = router
+    const responseObject = {
+      googleId: retrievedUser.googleId,
+      googleAccessToken: accessToken
+      links: retrievedUser.links
+    }
+
+    ctx.response.body = JSON.stringify(responseObject);
+  } catch (err) {
+    console.log(err);
+  }
+
+  return next();
+});
+
+module.exports = router;
--- server/models/user.js
+++ server/models/user.js
@@ -1,42 +1,51 @@
-const mongoose = require('mongoose')
-const findOrCreate = require('mongoose-find-or-create')
+const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-find-or-create");
 
 const userSchema = new mongoose.Schema({
-  email: String,
   googleId: String,
+  googleRefreshToken: String,
   links: [mongoose.Schema.Types.ObjectId]
-})
-userSchema.plugin(findOrCreate)
+});
+userSchema.plugin(findOrCreate);
 
-const User = mongoose.model('User', userSchema)
-exports.User = User
-
+const User = mongoose.model("User", userSchema);
+exports.User = User;
 
 exports.add = () => {
-  const user1 = new User({ email: 'email1' });
-  user1.save(function (err) {
+  const user1 = new User({ email: "email1" });
+  user1.save(function(err) {
     if (err) return handleError(err);
     // saved!
   });
-}
+};
 
 exports.getAll = async () => {
-  const users = await User.find({})
-  console.log(users)
-  return users
-}
+  const users = await User.find({});
+  console.log(users);
+  return users;
+};
 
-exports.findOrCreate = ({ userId, userEmail }) => {
-  return new Promise ((resolve, reject) => {
-    User.findOrCreate({ googleId: userId }
-      , {
-        googleId: userId,
-        email: userEmail,
+exports.setUserField = (googleId, field, value) => {
+  User.update({ googleId }, { [field]: value });
+};
+
+// Every google account has a unique id which is sub field in decoded token
+exports.findOrCreateByGoogleId = googleId => {
+  return new Promise((resolve, reject) => {
+    User.findOrCreate(
+      { googleId: googleId },
+      {
+        googleId: googleId,
+        googleAccessToken: null,
         links: []
-      }, (err, result) => {
-      // my new or existing model is loaded as result
-      if (err) { reject(err) } 
-      resolve(result)
-    })
-  })
-}
+      },
+      (err, result) => {
+        // my new or existing model is loaded as result
+        if (err) {
+          reject(err);
+        }
+        resolve(result);
+      }
+    );
+  });
+};
