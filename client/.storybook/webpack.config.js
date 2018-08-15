3d931f69ab5694cf7dd48f0b7cdf728ff4d56621
Dan <danpopa1998@gmail.com>
2018-08-15 21:06:20 +0300

    make docz work

--- client/.storybook/webpack.config.js
+++ client/.storybook/webpack.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const TSDocgenPlugin = require("react-docgen-typescript-webpack-plugin");
 module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
