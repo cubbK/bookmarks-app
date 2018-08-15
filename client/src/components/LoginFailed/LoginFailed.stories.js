550a65abca612fcf1b9eaed5cda36503c11a10fe
Dan <danpopa1998@gmail.com>
2018-08-15 22:44:39 +0300

    remove storybook config

--- client/src/components/LoginFailed/LoginFailed.stories.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import LoginFailed from "./LoginFailed.tsx";
-
-storiesOf("LoginFailed", module).add("default", () => <LoginFailed />);
