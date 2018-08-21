75786d461d2dc950b1f35434dae23b5a4fb483cc
Dan <danpopa1998@gmail.com>
2018-08-21 21:45:15 +0300

    sff redux Provider to docz wrapper

--- client/src/doczConfigFiles/wrapper.js
+++ client/src/doczConfigFiles/wrapper.js
@@ -3,10 +3,18 @@ import { muiTheme, styledTheme } from "theme/theme";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { ThemeProvider } from "styled-components";
 
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "store";
+
 const Wrapper = props => (
-  <MuiThemeProvider theme={muiTheme}>
-    <ThemeProvider theme={styledTheme}>{props.children}</ThemeProvider>
-  </MuiThemeProvider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <MuiThemeProvider theme={muiTheme}>
+        <ThemeProvider theme={styledTheme}>{props.children}</ThemeProvider>
+      </MuiThemeProvider>
+    </PersistGate>
+  </Provider>
 );
 
 export default Wrapper;
